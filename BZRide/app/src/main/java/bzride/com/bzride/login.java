e7a69ec3d57002041fee164a1149757514c4a6af
sankjoseph <sankjoseph@gmail.com>
2016-06-16 09:22:39 +0530

    login change
    login

--- BZRide/app/src/main/java/bzride/com/bzride/login.java
+++ BZRide/app/src/main/java/bzride/com/bzride/login.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class login extends AppCompatActivity {
@@ -14,8 +16,17 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        EditText loginemailtext = (EditText)findViewById(R.id.txtemail);
-        EditText passwordtext = (EditText)findViewById(R.id.txtpassword);
+
+        final EditText loginemailtext = (EditText)findViewById(R.id.txtemail);
+        final EditText passwordtext = (EditText)findViewById(R.id.txtpassword);
+        Button btnLogin  = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener( new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Log.v("EditText", loginemailtext.getText().toString());
+            }
+        });
 
     }
 
