1c588db12e0271cda6abb084760748bb9f3c7097
sankjoseph <sankjoseph@gmail.com>
2016-08-25 21:58:29 +0530

    notification code
    code for push

--- web/bzride/RequestHandler.php
+++ web/bzride/RequestHandler.php
@@ -18,17 +18,105 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
 		$deviceId = $row["DeviceId"];
 		$DeviceType = $row["DeviceType"];
-		if ($deviceId == 'A')
+		if ($DeviceType == 'A')
 		{
 			//Android notification
+			$devicetoken = '';  // Give device token here.
+			$pushMessage = ''; // Give push message here.
+			$apikey = ''; // Give api key here.
+
+			androidpush($devicetoken,$pushMessage,$apikey);
+
 		}
-		else if ($deviceId == 'I')
+		else if ($DeviceType == 'I')
 		{
 			//ios notification
+			$devicetoken = '';  // Give device token here.
+			$from = ''; // Give from name.
+			$fromname = ''; // Give from.
+			apns($deviceToken,$from,$fromname)
 		}
     }
 } else {
    showError("Could not find any drivers avilable at this time. Please retry.");
+}
+
+function androidpush($devicetoken,$pushMessage,$apikey)
+{
+      $gcmRegID  = $devicetoken;
+    if (isset($gcmRegID) && isset($pushMessage)) {    
+            $registatoin_ids = array($gcmRegID);
+            $message = array("m" => $pushMessage,'x' => 'call');
+            
+            $url = 'https://android.googleapis.com/gcm/send';
+          $fields = array(
+             'registration_ids' => $registatoin_ids,
+             'data' => $message,
+             'priority' => 'high'
+          );
+          define("GOOGLE_API_KEY", "$apikey");    
+          $headers = array(
+              'Authorization: key=' . GOOGLE_API_KEY,
+              'Content-Type: application/json'
+          );
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_URL, $url);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+          $result = curl_exec($ch);       
+          curl_close($ch);
+    }
+    return true;
+}
+
+function apns($deviceToken,$from,$fromname)
+{
+      $passphrase = 'xxxxxxx';      
+      $message['loc-key'] = "IC_MSG";
+      $message['message'] = "$fromname";
+      
+      $ctx = stream_context_create();
+      stream_context_set_option($ctx, 'ssl', 'local_cert', 'production.pem');
+      stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+      
+      // Open a connection to the APNS server
+      $fp = stream_socket_client(
+          'ssl://gateway.push.apple.com:2195', $err,
+          $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+      
+      if (!$fp)
+		  showError("Failed to connect. Please retry.");
+          //exit("Failed to connect: $err $errstr" . PHP_EOL);
+      else
+	  {
+		  showSuccess("Connected to APNS.");
+	  }
+      
+      $body['aps'] = array(
+          'alert' => "You have a ride request from ".$fromname,
+          'sound' => "default"
+    );
+      
+      // Encode the payload as JSON
+      $payload = json_encode($body);
+      
+      // Build the binary notification
+      $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+      
+      // Send it to the server
+      $result = fwrite($fp, $msg, strlen($msg));
+            
+      if (!$result)
+		   showError("Ride request message not delivered. Please retry.");
+      else
+		  showSuccess("Ride request message successfully delivered. Please retry.");
+      
+      // Close the connection to the server
+      fclose($fp);
 }
 
 ?>
